95331e071a8a67248c675cbb37f23cacffa4d702
dimapb <1st.trillionaire.dm@gmail.com>
2024-04-28 16:52:41 +0300

    новая версия, функциональный подход

--- src/main.py
+++ src/main.py
@@ -1,18 +1,26 @@
 #! /home/dimapb/.cache/pypoetry/virtualenvs/src-PhVsYnOQ-py3.10/bin/python
-
-from funcs import get_data, Operation
+from os.path import abspath
+from funcs import get_json, rm_broken_data, filter_by_state, sort_by_date, get_number_operation  
+from utils import format_datetime, get_amount, security
 
 
 def main():
-    operations = get_data("operations.json")
+    data = get_json(abspath("./src/operations.json"))
 
-    for row in operations:
-        operation = Operation(row)
-        print(operation)
+    data = rm_broken_data(data)
+
+    data = filter_by_state(data)
+    
+    data = sort_by_date(data)
+
+    data = get_number_operation(data)
+    
+    for row in data:
+        print(
+f"""{format_datetime(row['date'])} {row['description']}
+{security(row['from']) + ' -> ' if 'from' in row.keys() else ''}{security(row['to'])}
+{get_amount(row['operationAmount'])}"""
+             )
         print()
 
-
 main()
-
-
-
